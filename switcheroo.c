a9aa6e0e80dae258d33dd8b25d5bb7f750045b80
shane-decatur <sdecatur20@stuy.edu>
2021-10-02 20:21:51 -0400

    initalized second array in reverse order of first using both [] and *

--- switcheroo.c
+++ switcheroo.c
@@ -16,5 +16,22 @@ int main(){
   for (i = 0; i < 9; i++){
     printf("%d, ",arr1[i]);
   }
-  printf("%d]",arr1[9]);
+  printf("%d]\n\n",arr1[9]);
+
+  int arr2[10];
+  int *onep = arr1;
+  int *twop = arr2;
+
+  for (i = 0; i < 10; i++){
+    twop[i] = onep[9-i];
+  }
+  for (i = 0; i < 10; i++){
+    *(twop+i) = *(onep+9-i);
+  }
+
+  printf("[");
+  for (i = 0; i < 9; i++){
+    printf("%d, ",arr2[i]);
+  }
+  printf("%d]\n\n",arr2[9]);
 }
