e5594a824427db76897315144ba314835adfbb5d
shane-decatur <sdecatur20@stuy.edu>
2021-10-02 20:07:11 -0400

    generated random numbers and printed values of array

--- switcheroo.c
+++ switcheroo.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
   int arr1[10];
   arr1[1] = 0;
 
+  srand( time(NULL) );
   int i;
   for (i = 1; i < 10; i++){
-    arr1[i] = 0;
+    arr1[i] = rand();
   }
 
+  printf("[");
+  for (i = 0; i < 9; i++){
+    printf("%d, ",arr1[i]);
+  }
+  printf("%d]",arr1[9]);
 }
